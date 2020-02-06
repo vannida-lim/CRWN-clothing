574e85d562cdad50ea25e5967175781876e2215e
Vannida Lim <vannida.lim@gmail.com>
2020-02-06 00:03:07 -0500

    added shop page, collection preview, and collection item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css';
+
 import HomePage from './pages/homepage/HomePage'
+import ShopPage from './pages/shop/ShopPage'
 
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -43,7 +43,7 @@ class Directory extends Component{
     render(){
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>)}
+                {this.state.sections.map(({id, ...otherSectionProps }) => <MenuItem key={id} {...otherSectionProps}/>)}
             </div>
         )
     }
