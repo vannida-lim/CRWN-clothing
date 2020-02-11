6e97ba008fac0137f9336552fdc5bfcf1304430a
Vannida Lim <vannida.lim@gmail.com>
2020-02-11 16:23:55 -0500

    added cartIcon and cartDropdown components, implemented cartReducer

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,30 +4,33 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { auth } from '../../firebase/firebase.utilis'
+import CartIcon from '../../components/cart-icon/CartIcon'
+import CartDropdown from '../../components/cart-dropdown/CartDropdown'
 
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => {
-    return(
-        <div className='header'>
-            <Link className='logo-container' to='/'> 
-                <Logo className='logo'></Logo>
-            </Link>
-            <div className='options'>
-                <Link className='option' to='/shop'>SHOP</Link>
-                <Link className='option' to='/'>CONTACT</Link>
-                {
-                    currentUser ? 
-                    <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : 
-                    <Link className='option' to='/signin'>SIGN IN</Link>
-                }
-            </div>
+const Header = ({ currentUser, hidden }) => (
+    <div className='header'>
+        <Link className='logo-container' to='/'> 
+            <Logo className='logo'></Logo>
+        </Link>
+        <div className='options'>
+            <Link className='option' to='/shop'>SHOP</Link>
+            <Link className='option' to='/'>CONTACT</Link>
+            {
+                currentUser ? 
+                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : 
+                <Link className='option' to='/signin'>SIGN IN</Link>
+            }
+            <CartIcon/>
         </div>
-    )
-}
+        {hidden ? null: <CartDropdown/>}
+    </div>
+);
 
 const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+    currentUser: state.user.currentUser,
+    hidden: state.cart.hidden
 })
 
 export default connect(mapStateToProps)(Header);
---
+++ src/redux/cart/CartReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    hidden: true
+}
+
+const cartReducer = (state = initialState, { type, payload }) => {
+    switch(type){
+        case 'TOGGLE_CART_HIDDEN':
+            return {
+                ...state, hidden: !state.hidden
+            }
+        default:
+            return state;
+    }
+}
+
+export default cartReducer;
